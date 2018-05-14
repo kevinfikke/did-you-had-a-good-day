26bf52850218f7a37dc0edbbe5707756a17fcfe8
Unknown <timschachtschabel14@gmail.com>
2018-05-14 09:25:25 +0200

    school start

--- development/surveyapp/resources/views/welcome.blade.php
+++ development/surveyapp/resources/views/welcome.blade.php
@@ -2,30 +2,36 @@
 
 @section('content')
 <div class="container">
-<div class="content">
-		<form action="">
-			<button>
-				<div class="img">
-					<img src="img/smile.jpg">
-				</div>
-			</button>
-		</form>
-
-		<form action="">
-			<button>
-				<div class="img">
-					<img src="img/neutral.png">
-				</div>
-			</button>
-		</form>
-			
-		<form action="">	
+    <div class="content">
+	<form method="post" action="{{ route('vote') }}">
+		<input type="hidden" value="{{csrf_token()}}" name="_token"/>
 			<button>
 				<div class="img">
 					<img src="img/sad.png">
 				</div>
-			</button>
+            </button>
+            <input type="submit" class="form-control" name="1">
 		</form>
+
+		<form method="post" action="{{route('vote') }}">
+			<input type="hidden" value="{{csrf_token()}}" name="_token">
+			<button>
+				<div class="img">
+					<img src="img/neutral.png">
+				</div>
+            </button>
+            <input type="submit" class="form-control" name="2">
+		</form>
+			
+		<form method="post" action="{{ route('vote') }}">	
+			<input type="hidden" value="{{csrf_token()}}" name="_token"/>
+			<button>
+				<div class="img">
+					<img src="img/happy.png">
+				</div>
+            </button>
+            <input type="submit" class="form-control" name="3">
+        </form>
 	</div>
 </div>
 @endsection
